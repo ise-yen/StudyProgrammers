bdc4967d6705c39d285b3fed8bf3b9883f48a57c
YeEun <ydkrkek@gmail.com>
2023-09-20 22:34:35 +0900

    [Add] prms_87389_나머지가1이되는수찾기 Solved 2분, 구현

---
+++ programmersAlgo/programmersAlgo/MonthlyCodeChallenge/Season3/prms_87389_나머지가1이되는수찾기.cpp
@@ -0,0 +1,17 @@
+﻿// 나머지가 1이 되는 수 찾기
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1000001;
+
+    for (int x = 1; x < n; x++) {
+        if (n % x == 1) {
+            answer = answer < x ? answer : x;
+        }
+    }
+    return answer;
+}
