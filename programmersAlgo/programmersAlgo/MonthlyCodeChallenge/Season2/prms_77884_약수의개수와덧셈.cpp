107e562e4c5d445c6947e32d2e54ca9d7f2ec245
YeEun <ydkrkek@gmail.com>
2023-09-24 02:28:26 +0900

    [Add] prms_7784_약수의개수와덧셈 Solved 5분, 구현. 약수의 개수 찾을 때는 절반만 탐색

---
+++ programmersAlgo/programmersAlgo/MonthlyCodeChallenge/Season2/prms_77884_약수의개수와덧셈.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    for (int num = left; num <= right; num++) {
+        int N = (int)sqrt(num);
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            if (N % i != 0) continue;
+            // 약수의 개수
+            if (num == i * i) cnt++;
+            else cnt += 2;
+        }
+        if (cnt % 2 == 0) answer += num;
+        else answer -= num;
+    }
+    return answer;
+}
